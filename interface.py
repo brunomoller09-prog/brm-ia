6f843609bd3264fdb7bdd851d66dc94b1590013a
brunomoller09-prog <brunomoller09@gmail.com>
2026-05-21 09:12:37 -0300

    Update interface.py

--- interface.py
+++ interface.py
@@ -43,4 +43,8 @@ demo = gr.ChatInterface(
     description="Assistente de Processos"
 )
 
-demo.launch()
+import os
+
+port = int(os.environ.get("PORT", 7860))
+
+demo.launch(server_name="0.0.0.0", server_port=port)
