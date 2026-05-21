54763d447374f6d2497e22a0d0b705b108b5cdc8
brunomoller09-prog <brunomoller09@gmail.com>
2026-05-21 10:27:39 -0300

    Update interface.py

--- interface.py
+++ interface.py
@@ -58,5 +58,5 @@ app = gr.mount_gradio_app(app, demo, path="/")
 
 # ✅ execução
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 7860))
+    port = int(os.environ.get("PORT", 8080))
     uvicorn.run(app, host="0.0.0.0", port=port)
